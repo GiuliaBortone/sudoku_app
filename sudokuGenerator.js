39dab4b3ee741464eabb26ae4dbbcb3c475c4949
GiuliaBortone <giulia.bortone@mail.polimi.it>
2024-02-21 12:45:54 +0100

    fill remaining cells of sudoku matrix

--- sudokuGenerator.js
+++ sudokuGenerator.js
@@ -1,6 +1,7 @@
 function generateValidSudokuGrid() {
     let matrix = initialiseEmptyMatrix()
     fillMainDiagonalBoxes(matrix)
+    fillRemainingCells(matrix, 0, 3)
 
     return matrix
 }
@@ -47,4 +48,70 @@ function shuffleFisherYatesAlgorithm(array) {
     }
 
     return array;
+}
+
+function fillRemainingCells(matrix, startingRow, startingColumn) {
+    if (startingColumn >= 9) {
+        startingRow += 1
+        startingColumn = 0
+    }
+
+    if (startingRow >= 9) {
+        return true
+    }
+
+    if (matrix[startingRow][startingColumn] !== 0) {
+        return fillRemainingCells(matrix, startingRow, startingColumn + 1)
+    }
+
+    for (let newValue = 1; newValue <= 9; newValue++) {
+        if (canBeAdded(newValue, matrix, startingRow, startingColumn)) {
+            matrix[startingRow][startingColumn] = newValue
+
+            if (fillRemainingCells(matrix, startingRow, startingColumn + 1)) {
+                return true
+            }
+            matrix[startingRow][startingColumn] = 0
+        }
+    }
+
+    return false
+}
+
+function canBeAdded(newValue, matrix, startingRow, startingColumn) {
+    return isNotInRow(newValue, matrix, startingRow) &&
+        isNotInColumn(newValue, matrix, startingColumn) &&
+        isNotInBox(newValue, matrix, startingRow, startingColumn)
+}
+
+function isNotInRow(value, matrix, row) {
+    for (let column = 0; column < 9; column++) {
+        if (matrix[row][column] === value) {
+            return false
+        }
+    }
+    return true
+}
+
+function isNotInColumn(value, matrix, column) {
+    for (let row = 0; row < 9; row++) {
+        if (matrix[row][column] === value) {
+            return false
+        }
+    }
+    return true
+}
+
+function isNotInBox(value, matrix, row, column) {
+    const startingRow = row - (row % 3)
+    const startingColumn = column - (column % 3)
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (matrix[startingRow + i][startingColumn + j] === value) {
+                return false
+            }
+        }
+    }
+    return true
 }
